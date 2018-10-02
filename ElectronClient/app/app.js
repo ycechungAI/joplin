dea5d4f012cfcdeb5cc3cdf94b712d34344c849c
Laurent Cozic <laurent@cozic.net>
2018-10-02 08:40:33 +0100

    Electron: Fixes #798: Enable Select All shortcut in macOS

--- ElectronClient/app/app.js
+++ ElectronClient/app/app.js
@@ -419,6 +419,10 @@ class Application extends BaseApplication {
 					role: 'paste',
 					accelerator: 'CommandOrControl+V',
 				}, {
+					label: _('Select all'),
+					role: 'selectall',
+					accelerator: 'CommandOrControl+A',
+				}, {
 					type: 'separator',
 					screens: ['Main'],
 				}, {
