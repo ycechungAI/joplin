03adf7fc7c878f82f6a43515d37fd5e3dd59390f
Laurent Cozic <laurent@cozic.net>
2020-02-10 22:09:18 +0000

    Desktop: Adding Mermaid support

---
+++ ReactNativeClient/lib/joplin-renderer/assets/mermaid/mermaid_render.js
@@ -0,0 +1,15 @@
+console.info('BBBBBBBBBBBBBBBBBBBBBBBBBBB');
+
+alert('AAAAAAAAAAAAAAAAAAAAA');
+
+var config = {
+startOnLoad:true,
+flowchart:{
+  useMaxWidth:true,
+  htmlLabels:true,
+  curve:'cardinal',
+},
+
+securityLevel:'loose',
+};
+mermaid.initialize(config);
