ae8a0641ab3681a3b25f6dc4ac40f20d52aaeb4d
Laurent Cozic <laurent@cozic.net>
2020-02-11 17:58:08 +0000

    Fixed plugin asset

--- ReactNativeClient/lib/joplin-renderer/assets/mermaid/mermaid_render.js
+++ ReactNativeClient/lib/joplin-renderer/assets/mermaid/mermaid_render.js
@@ -1,15 +1,24 @@
-console.info('BBBBBBBBBBBBBBBBBBBBBBBBBBB');
+/* global mermaid */
 
-alert('AAAAAAAAAAAAAAAAAAAAA');
+function mermaidReady() {
+	return typeof mermaid !== 'undefined';
+}
 
-var config = {
-startOnLoad:true,
-flowchart:{
-  useMaxWidth:true,
-  htmlLabels:true,
-  curve:'cardinal',
-},
+function mermaidInit() {
+	// Mermaid's wonderful API has two init methods: init() and initialize().
+	// init() is deprectated but works, and initialize() is recommended but doesn't
+	// work, so let's use init() for now.
+	if (mermaidReady()) mermaid.init();
+}
 
-securityLevel:'loose',
-};
-mermaid.initialize(config);
+document.addEventListener('joplin-noteDidUpdate', () => {
+	mermaidInit();
+});
+
+const initIID_ = setInterval(() => {
+	const isReady = mermaidReady();
+	if (isReady) {
+		clearInterval(initIID_);
+		mermaidInit();
+	}
+}, 100);
